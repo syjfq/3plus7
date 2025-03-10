f8388627a296b04111f33d7a2522737fdb0162c5
syjfq <3507883719@qq.com>
2025-03-10 14:44:41 +0800

    Clear cache

--- os-workbench/kernel/build/x86_64-qemu/framework/main.d
+++
@@ -1,6 +0,0 @@
-/home/ywh/os-workbench/kernel/build/x86_64-qemu/framework/main.o: \
- /home/ywh/os-workbench/kernel/framework/main.c framework/kernel.h \
- /home/ywh/os-workbench/kernel/../abstract-machine/am/include/am.h \
- /home/ywh/os-workbench/kernel/../abstract-machine/am/include/arch/x86_64-qemu.h \
- /home/ywh/os-workbench/kernel/../abstract-machine/am/include/amdev.h \
- /home/ywh/os-workbench/kernel/../abstract-machine/klib/include/klib.h
--- os-workbench/kernel/build/x86_64-qemu/src/spinlock.d
+++
@@ -1,8 +0,0 @@
-/home/ywh/os-workbench/kernel/build/x86_64-qemu/./src/spinlock.o: \
- /home/ywh/os-workbench/kernel/src/spinlock.c include/common.h \
- framework/kernel.h \
- /home/ywh/os-workbench/kernel/../abstract-machine/am/include/am.h \
- /home/ywh/os-workbench/kernel/../abstract-machine/am/include/arch/x86_64-qemu.h \
- /home/ywh/os-workbench/kernel/../abstract-machine/am/include/amdev.h \
- /home/ywh/os-workbench/kernel/../abstract-machine/klib/include/klib.h \
- /home/ywh/os-workbench/kernel/../abstract-machine/klib/include/klib-macros.h
